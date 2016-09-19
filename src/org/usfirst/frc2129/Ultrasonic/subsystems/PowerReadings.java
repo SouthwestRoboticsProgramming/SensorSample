59795a992384030b503a59b8f9e60e5b5207fde0
Steve Chin <nevsdea-junk@earthlink.net>
2016-09-18 22:37:00 -0500

    Initial commit of sensor sample

---
+++ src/org/usfirst/frc2129/Ultrasonic/subsystems/PowerReadings.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc2129.Ultrasonic.subsystems;
+
+public class PowerReadings {
+	public double temperature;
+	public double totalCurrent;
+	public double totalEnergy;
+	public double totalPower;
+	public double voltage;
+}
